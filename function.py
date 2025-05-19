0ccafb81c8ef1a6fa6c2f3d56c0da24bcf02076b
Md.Reyajul Islam <mdreyajul385@gmail.com>
2025-05-20 04:28:41 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,23 @@
+#function defination
+# def calc_sum(a,b): #(a,b)-parameters
+#     return a+b
+
+# sum = calc_sum(5,10) #calc_sum() - function call; (5,10)-arguments
+# print(sum)
+
+# calc_sum(5,105)
+# calc_sum(15,110)
+# calc_sum(52,12)
+
+
+
+
+#list types of function  
+cities = ["Dhaka","Khulna","Sylet","Mymensingh","Borishal"]
+village = ["Khusli","Trishal","Sunamgonj","Mothbariha","Boira"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(village)    
